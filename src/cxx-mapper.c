a8f02b269fc00e820792421d38780b7e30265eb1
Nathan Sidwell <nathan@acm.org>
2019-01-28 15:38:13 -0500

    Create initial C++ mapper files
    	src/cxx-mapper.c: The module mapper module
    	configure.ac: Configurery enablement
    	Makefile.am: Add source file

---
+++ src/cxx-mapper.c
@@ -0,0 +1,30 @@
+/* C++ Module Mapper Machinery.  Experimental!
+Copyright (C) 1988-2018 Free Software Foundation, Inc.
+Written by Nathan Sidwell <nathan@acm.org> while at FaceBook
+
+This file is part of GNU Make.
+
+GNU Make is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+GNU Make is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "makeint.h"
+#include <stdio.h>
+
+#ifdef HAVE_FCNTL_H
+# include <fcntl.h>
+#elif defined(HAVE_SYS_FILE_H)
+# include <sys/file.h>
+#endif
+
+#ifdef MAKE_CXX_MAPPER
+
+#endif /* MAKE_CXX_MAPPER */
